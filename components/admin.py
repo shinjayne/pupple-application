242656620d2130087576bdaa46572c26fafd1341
stopsilver13 <stopsilver13>
2020-08-14 00:13:49 +0900

    contents, components admin 초안 작성

--- components/admin.py
+++ components/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from components.models import LookItemInfoComponent, ItemCategoryInfoComponent, VoteComponent, VoteChoice
 
-# Register your models here.
+admin.site.register(LookItemInfoComponent)
+admin.site.register(ItemCategoryInfoComponent)
+
+# class LookItemInfoComponentInline(admin.StackedInline):
+#     model = LookItemInfoComponent
+
+# class ItemCategoryInfoComponentInline(admin.StackedInline):
+#     model = ItemCategoryInfoComponent
+
+class VoteChoiceInline(admin.StackedInline):
+    model = VoteChoice
+
+@admin.register(VoteComponent)
+class VoteComponentAdmin(admin.ModelAdmin):
+    inlines = [VoteChoiceInline]
+
+# class VoteComponentInline(admin.StackedInline):
+#     model = VoteComponent
--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contents.models import ShoppableContents, YoutubeContents, Creator, Item, Look
 
-# Register your models here.
+admin.site.register(ShoppableContents)
+admin.site.register(Creator)
+admin.site.register(Item)
+
+class LookInline(admin.StackedInline):
+    model = Look
+    readonly_fields = ('like',)
+
+@admin.register(YoutubeContents)
+class YoutubeContentsAdmin(admin.ModelAdmin):
+    inlines = [LookInline]
