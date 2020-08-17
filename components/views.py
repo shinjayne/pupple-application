5b87937d1f332ab735698a227e1910facc48785b
stopsilver13 <stopsilver13>
2020-08-17 17:30:14 +0900

    view API 초안 작성

--- components/views.py
+++ components/views.py
@@ -1,3 +1,54 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from components.models import Component, LookItemInfoComponent, ItemCategoryInfoComponent, VoteComponent
+
+def component_to_response(pk):
+    component_class = Components.objects.get_subclass(pk=pk)
+    component = Components.objects.get(pk=pk)
+
+    if component_class == "LookItemInfoComponent" or "ItemCategoryInfoComponent":
+        component_info = basic_info_component_to_response(component)
+    elif component_class == "VoteComponent":
+        component_info = vote_component_to_response(component)
+    else:
+        return JsonResponse(status=204)
+
+    response = {
+        "type": component_class,
+        "fields": component_info,
+    }
+
+    return JsonResponse(reponse)
+
+def basic_info_component_to_response(component):
+    response = {
+        "title": component.title,
+        "explain": component.explain,
+        "want_to_promote": component.want_to_promote,
+    }
+
+    return response
+
+def vote_component_to_response(component):
+    choices = component.vote_choice_set.all()
+    basic_info = basic_info_component_to_response(component)
+    add_info = {
+        "img_url": component.img.url,
+        "choices": [
+            vote_component_to_response(choice) for choice in vote_component_to_response
+        ]
+    }
+
+    response = basic_info.update(add_info)
+
+    return response
+
+def vote_component_choice_to_response(choice):
+    response = {
+        "name": choice.name,
+        "img_url": choice.img.url,
+        "vote": choice.vote,
+    }
+
+    return response
--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,73 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from contents.models import ShoppableContents, YoutubeContents, Look, Item
+
+def shoppable_contents_to_response(pk):
+    shoppable_contents = ShoppableContents.objects.get(pk=pk)
+    youtube_contents = shoppable_contents.youtube_contents_set.all()
+    components = shoppable_contents.component_set.all()
+
+    response = {
+        "title": shoppable_contents.title,
+        "explain": shoppable_contents.explain,
+        "img_url": shoppable_contents.background_img.url,
+        "youtube_contents_pk_list": [
+            yotube_content.pk for youtube_content in youtube_contents
+        ],
+        "components_pk_list": [
+            component.pk for component in components
+        ],
+    }
+
+    return JsonResponse(response)
+
+def youtube_contents_to_response(pk):
+    youtube_contents = YoutubeContents.objects.get(pk=pk)
+    creator = youtube_contents.creator
+    looks = youtube_contents.look_set.all()
+
+    response = {
+        "title": youtube_contents.title,
+        "creator": {
+            "name": creator.name,
+            "explain": creator.explain,
+            "profile_img_url": creator.profile_img.url
+        },
+        "link": youtube_contents.link,
+        "looks_pk_list": [
+            look.pk for look in looks
+        ],
+    }
+
+    return JsonResponse(response)
+
+def looks_to_response(pk):
+    look = Look.objects.get(pk=pk)
+    items = look.item_set.all()
+
+    response = {
+        "title": look.title,
+        "main_img_url": look.main_img.url,
+        "like": look.like,
+        "items_pk_list": [
+            item.pk for item in items
+        ],
+    }
+
+    return JsonResponse(response)
+
+def items_to_response(pk):
+    item = Item.objects.get(pk=pk)
+
+    response = {
+        "name": item.name,
+        "category": item.category,
+        "explain": item.explain,
+        "main_img_url": item.main_img.url,
+        "price": item.price,
+        "link": item.link,
+    }
+
+
+
