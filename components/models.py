0b9c9414825050ff08e550f2bcf30b27cb7de3df
stopsilver13 <stopsilver13>
2020-08-17 17:27:00 +0900

    components model 수정(related_name 추가)

--- components/models.py
+++ components/models.py
@@ -9,7 +9,7 @@ def vote_choice_img_upload_path(instance):
     return "component/vote/{}".format(instance.vote_component.title)
 
 class Component(models.Model):
-    shoppable_contents = models.ForeignKey(ShoppableContents, on_delete=models.CASCADE)
+    shoppable_contents = models.ForeignKey(ShoppableContents, on_delete=models.CASCADE, related_name="component_set")
     title = models.CharField(max_length=100)
     explain = models.TextField(blank=True)
     want_to_promote = models.BooleanField(default=False)
@@ -28,7 +28,7 @@ class VoteComponent(Component):
     img = models.ImageField(upload_to=vote_img_upload_path, blank=True)
 
 class VoteChoice(models.Model):
-    vote_component = models.ForeignKey(VoteComponent, on_delete=models.CASCADE)
+    vote_component = models.ForeignKey(VoteComponent, on_delete=models.CASCADE, related_name="vote_choice_set")
     name = models.CharField(max_length=100, blank=True)
     img = models.ImageField(upload_to=vote_choice_img_upload_path, blank=True)
     vote = models.PositiveIntegerField(default=0)
