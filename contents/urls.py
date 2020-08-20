ffe56546b1cae027e3f920c7d6bb7a403cc4b2d3
stopsilver13 <stopsilver13>
2020-08-20 20:39:51 +0900

    view API 수정에 따른 urls.py 수정

--- contents/urls.py
+++ contents/urls.py
@@ -3,7 +3,5 @@ from contents import views
 
 urlpatterns = [
     path('<int:pk>/', views.shoppable_contents_to_response, name='shoppable_contents_to_response'),
-    path('youtube/<int:pk>/', views.youtube_contents_to_response, name='youtube_contents_to_response'),
-    path('look/<int:pk>/', views.looks_to_response, name='looks_to_response'),
     path('item/<int:pk>/', views.items_to_response, name='items_to_response'),
 ]
