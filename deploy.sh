158dfea307816df3ba4afbcd0565be10b8bcc6e3
Jayne Shin <shinjayne@gmail.com>
2020-08-25 21:37:03 +0900

    deploy.sh 수정하고 주석 추가함.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,19 @@
-sudo git pull origin
+# install host machine requirements.
 sudo apt-get -y install pipenv
 sudo apt-get -y install nginx
+pip install gunicorn
+
+# prepare source code
+sudo git pull origin
+
+# get into python virtual environment
 pipenv shell
+
+# install dependencies from Pipfile
 pipenv install
+# collect static
 python manage.py collectstatic
+# gunicorn setting & nginx setting
 sudo cp ./gunicorn.service /etc/systemd/system/gunicorn.service
 sudo cp ./nginx.conf /etc/nginx/sites-enabled/nginx.conf
 
