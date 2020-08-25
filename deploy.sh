bee4c66030b7fe7f5710f2860fc81ff7f82243c7
Jayne Shin <shinjayne@gmail.com>
2020-08-25 22:17:26 +0900

    gunicorn 실행 스크립트를 수정함

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,11 @@ echo 'install host machine requirements.'
 echo '########################'
 sudo apt-get -y install pipenv
 sudo apt-get -y install nginx
-pip install gunicorn
 
 echo '########################'
 echo 'prepare source code'
 echo '########################'
 sudo git pull origin
-
-echo '########################'
-echo 'get into python virtual environment'
-echo '########################'
-pipenv shell
 
 echo '########################'
 echo 'install dependencies from Pipfile'
@@ -24,7 +18,7 @@ pipenv install
 echo '########################'
 echo 'collect static'
 echo '########################'
-python manage.py collectstatic --noinput
+pipenv run python manage.py collectstatic --noinput
 
 echo '########################'
 echo 'gunicorn setting & nginx setting'
