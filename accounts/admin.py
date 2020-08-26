1254525d410da1f54f1a7ea36e96e4c413f2964f
stopsilver13 <stopsilver13>
2020-08-26 23:35:46 +0900

    accounts model 작성

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from accounts.models import IPUserProfile
 
-# Register your models here.
+admin.site.register(IPUserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from contents.models import Look
+from components.models import VoteChoice
+
+
+class IPUserProfile(models.Model):
+    ip_address = models.GenericIPAddressField()
+    liked_looks = models.ManyToManyField(Look, blank=True)
+    voted_choices = models.ManyToManyField(VoteChoice, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
